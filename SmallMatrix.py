644d92870492ce66a268ee2b5a814d71d1b0294e
lucakoecher-ltts <luca.koecher@ltts.com>
2025-10-27 08:50:59 +0100

    Bugfix 12483

--- SmallMatrix.py
+++ SmallMatrix.py
@@ -5,8 +5,10 @@ def testFunction():
     print("Hello")
     a = np.random.rand(2,3)
     b = np.random.rand(3,2)
+    print("a = ", a)
+    print("b = ", b)
     c = a.dot(b)
-    print(c)
+    print("result = ", c)
 
 
 if __name__ == "__main__":
